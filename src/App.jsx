9a5e74788d070631802e2c57380476a4b823c197
Khizar Wakeel <khizarwakeel@gmail.com>
2023-07-26 17:04:11 +0500

    finalcommit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     <div>
       <Navbar />
       <Herosection />
-      {/* <LiveAuction/> */}
+      <LiveAuction />
       <CreativeArtist />
       <MostPopular />
       <ExclusiveArtist />
--- src/components/liveauctions/liveauction.jsx
+++ src/components/liveauctions/liveauction.jsx
@@ -8,19 +8,19 @@ import { Pagination } from "swiper/modules";
 const LiveAuction = () => {
   return (
     <>
-      <div>
+      <div className="bg-[url('/assets/technologysols.png')]">
         <Swiper
           breakpoints={{
             1024: {
-              width: 1020,
+              width: 1160,
               slidesPerView: 4,
             },
             768: {
-              width: 768,
-              slidesPerView: 2,
+              width: 900,
+              slidesPerView: 3,
             },
             425: {
-              width: 425,
+              width: 300,
               slidesPerView: 1,
             },
             375: {
@@ -37,47 +37,283 @@ const LiveAuction = () => {
             clickable: true,
           }}
           modules={[Pagination]}
-          className="mx-auto w-[76%]"
+          className="mySwiper xl:mx-20 xl:py-20 lg:py-20 lg:mx-24 mx-1 md:px-5 md:pt-5 px-4 pt-5 text-white"
         >
-          <SwiperSlide className="text-center bg-[#191919] rounded-md text-white  w-full px-10">
-            <img src="/assets/auction-1.jpg" alt="" />
-            <h1>Product Development</h1>
-            <p>
-              There are many variations of passages of Lorem Ipsum available but
-              the majority have suffered.
-            </p>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-1.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar1.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
           </SwiperSlide>
-          <SwiperSlide className="text-center bg-white rounded-md">
-            <img src="/assets/auction-1.jpg" alt="" className="p-6" />
-            <h1 className="mb-3 text-2xl font-bold">Product Development</h1>
-            <p className="mx-4 text-slate-600 mb-14">
-              There are many variations of passages of Lorem Ipsum available but
-              the majority have suffered.
-            </p>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-2.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar2.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
           </SwiperSlide>
-          <SwiperSlide className="text-center bg-white rounded-md">
-            <img src="/assets/auction-1.jpg" alt="" className="p-6" />
-            <h1 className="mb-3 text-2xl font-bold">Product Development</h1>
-            <p className="mx-4 text-slate-600 mb-14">
-              There are many variations of passages of Lorem Ipsum available but
-              the majority have suffered.
-            </p>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-3.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar3.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
           </SwiperSlide>
-          <SwiperSlide className="text-center bg-white rounded-md">
-            <img src="/assets/auction-1.jpg" alt="" className="p-6" />
-            <h1 className="mb-3 text-2xl font-bold">Product Development</h1>
-            <p className="mx-4 text-slate-600 mb-14">
-              There are many variations of passages of Lorem Ipsum available but
-              the majority have suffered.
-            </p>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-4.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar4.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
           </SwiperSlide>
-          <SwiperSlide className="text-center bg-white rounded-md">
-            <img src="/assets/auction-1.jpg" alt="" className="p-6" />
-            <h1 className="mb-3 text-2xl font-bold">Product Development</h1>
-            <p className="mx-4 text-slate-600 mb-14">
-              There are many variations of passages of Lorem Ipsum available but
-              the majority have suffered.
-            </p>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-1.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar5.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-2.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar6.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-3.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar7.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-4.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar8.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-1.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar1.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-2.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar2.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-3.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar3.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="bg-[#212529] rounded-xl p-7">
+            <img
+              src="/assets/auction-4.jpg"
+              alt=""
+              className="mx-auto mb-5 rounded-md"
+            />
+            <h1 className="mb-3 text-2xl font-semibold">Virtual Worlds</h1>
+            <div className="flex items-center">
+              <div>
+                <img
+                  src="/assets/avatar4.jpg"
+                  className="rounded-full w-16 h-16"
+                />
+              </div>
+              <div className="ml-2 font-semibold">
+                <h1>@Richard</h1>
+              </div>
+            </div>
+            <div className="flex justify-between mt-5 font-semibold">
+              <h3>1.5 BNB</h3>
+              <h3>1 of 1</h3>
+            </div>
           </SwiperSlide>
         </Swiper>
       </div>
